ab026928ee9e894ad216031b9df446a800bc4fea
Hasan F <hfidan006@gmail.com>
2022-03-11 10:16:14 +0100

    Initial commit

---
+++ src/test/java/getHttpRequest/GetRequest01.java
@@ -0,0 +1,26 @@
+package getHttpRequest;
+
+
+public class GetRequest01 {
+
+/*
+    http://dummy.restapiexample.com/api/v1/employees  url'ine
+    GET request'i yolladigimda gelen response'un
+    status kodunun 200 ve content type'inin "application/json"
+    ve employees sayisinin 24
+    ve employee'lerden birinin "Ashton Cox"
+    ve gelen yaslar icinde 21, 61, ve 23 degerlerinden birinin oldugunu test edin.
+    */
+
+
+
+
+
+
+
+
+
+
+
+
+}
