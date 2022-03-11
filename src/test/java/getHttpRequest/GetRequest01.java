ee88efd868947f9d1c46f29c9368e8e2fd1bbada
Hasan F <hfidan006@gmail.com>
2022-03-11 11:33:43 +0100

    1103

--- src/test/java/getHttpRequest/GetRequest01.java
+++ src/test/java/getHttpRequest/GetRequest01.java
@@ -5,6 +5,7 @@ import baseUrl.DummyBaseUrl;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.junit.Assert;
 import org.junit.Test;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
@@ -14,7 +15,9 @@ public class GetRequest01 extends DummyBaseUrl {
 /*
     http://dummy.restapiexample.com/api/v1/employees  url'ine
     GET request'i yolladigimda gelen response'un
+
     status kodunun 200 ve content type'inin "application/json"
+
     ve employees sayisinin 24
     ve employee'lerden birinin "Ashton Cox"
     ve gelen yaslar icinde 21, 61, ve 23 degerlerinden birinin oldugunu test edin.
@@ -26,11 +29,9 @@ public class GetRequest01 extends DummyBaseUrl {
 
         spec.pathParams("parametre1","api","parametre2","v1","parametre3","employees");
 
-
         Response response = given().contentType(ContentType.JSON).spec(spec).when().get("/{parametre1}/{parametre2}/{parametre3}");
 
         response.prettyPrint();
-
 
         // headers kisminin testi
         response.then().assertThat().statusCode(200).contentType("application/json"); // header kismindaki status code ve content type testi yapildi.
@@ -41,23 +42,18 @@ public class GetRequest01 extends DummyBaseUrl {
 
 
 
-
-
-
-
-
-
-
-
         //body kisminin testi.
+
         JsonPath jsonPath = response.jsonPath(); // burada sadece body icerigi var.
 
+        Assert.assertEquals(24, jsonPath.getList("data.employee_name").size());
 
+        //     ve employee'lerden birinin "Ashton Cox"
 
+        Assert.assertTrue(jsonPath.getList("data.employee_name").contains("Ashton Cox"));
 
-
-
-
+        Assert.assertEquals(200, response.getStatusCode());
+        Assert.assertEquals("application/json", response.getContentType());
 
 
 
